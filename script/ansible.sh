2c31ac7b16141c28bb3cc85b5d9f8f61d7dff3af
Jason <jason.sinc95@gmail.com>
2020-06-09 15:59:51 +0000

    Ready for deployment test

---
+++ script/ansible.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo source ~/.bashrc
+ansible-playbook -i inventory.cfg playbook.yml
--- script/installation.sh
+++ script/installation.sh
@@ -11,5 +11,5 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo systemctl status docker
 sudo usermod -aG docker $USER
-
-
+pip install --user ansible
+ansible --version
