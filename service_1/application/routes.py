2f897e7c38ea1e5a01303618d762f5bd738807de
Jason <jason.sinc95@gmail.com>
2020-06-11 14:48:35 +0000

    project complete

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -9,5 +9,4 @@ from application.models import CarConfig
 def home():
         carconfigData = CarConfig.query.all()
         response = requests.get('http://service4:5003/').text
-        generate_vehicle_config = response.text
-        return render_template('home.html', generate_vehicle_config = generate_vehicle_config, car=carconfigData, title='Home')
+        return render_template('home.html', car=response, title='Home')
