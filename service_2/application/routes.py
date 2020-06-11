7c2e9c22a984583ede967274dcae4cfc1d4a8b6a
Jason <jason.sinc95@gmail.com>
2020-06-11 16:09:22 +0000

    project complete

--- service_2/application/routes.py
+++ service_2/application/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, Flask, request, url_for, redirect, Response
 from application import app 
 import requests
 from random import randint
+from flask_sqlalchemy import SQLAlchemy
 
 @app.route('/', methods=['GET', 'POST'])
 def car():
--- service_3/application/routes.py
+++ service_3/application/routes.py
@@ -2,13 +2,7 @@ from flask import render_template, Flask, request, url_for, redirect, Response
 from application import app 
 import requests
 from random import randint 
-
-
-
-#@app.route('/car_weapon', methods=['GET'])
-#def car_weapon():
-#    weapon = ['oil spill', 'machine gun', 'EMP', 'Rockets', 'Nitrous', 'Invisibility cloak']
-#    return Response(weapon[randint(0,5)], mimetype='text/plain')
+from flask_sqlalchemy import SQLAlchemy
 
 @app.route('/', methods=['GET', 'POST'])
 def weapon():
