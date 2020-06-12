c6ac18f318bb20c1c54e6b857eec1e7c6eaa408f
Jason <jason.sinc95@gmail.com>
2020-06-12 11:03:32 +0000

    crud functionality attempted

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -16,8 +16,9 @@ def home():
 @app.route('/Delete/<id>/CarConfig/', methods=['GET','POST'])
 def DeleteCarConfig(id):
 	carconfigData = CarConfig.query.filter_by(id=id).first()
-	allconfigs = CarConfig.query.all()
-	for c in allconfigs:
-		db.session.delete(c)
+	
+	
+	db.session.delete(job_post)
+
 	db.session.commit()
 	return redirect(url_for('home'))
